c50cc4f4c3e71353ebf83d7eb4353908294c3759
Mehek <21cse316.mehekswain@giet.edu>
2024-06-30 22:01:03 +0530

    FindMedianSortedArray

--- qn4.py
+++ qn4.py
@@ -14,9 +14,9 @@ def findMedianSortedArrays(nums1,nums2):
         j = half - i -2   # left partition of B
 
         Aleft = A[i] if i >= 0 else float("-infinity")
-        Aright = A[i + 1] if i < len(A) else float("infinity")
+        Aright = A[i + 1] if (i+1) < len(A) else float("infinity")
         Bleft = B[j] if j >= 0 else float("-infinity")
-        Bright = B[j + 1] if j < len(B) else float("infinity")
+        Bright = B[j + 1] if (j+1) < len(B) else float("infinity")
 
         # Checking for correctness of left partition
         if Aleft <= Bright and Bleft <= Aright:
@@ -24,7 +24,7 @@ def findMedianSortedArrays(nums1,nums2):
             if total % 2:
                 return min(Aright,Bright)
             # even condition
-            return (max(Aleft,Bleft) + min(Aright + Bright)) / 2
+            return (max(Aleft,Bleft) + min(Aright , Bright)) / 2
         
         elif Aleft > Bright:
             r = i - 1
@@ -32,6 +32,8 @@ def findMedianSortedArrays(nums1,nums2):
         else:
             l = i + 1
 
+result=findMedianSortedArrays([1,3],[2])
+print(result)
             
 
 
